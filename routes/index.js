8e53aa5172e7ce133d1955eb9c2a5342a014e8a9
Ari Wiradana <ariwiradana5@gmail.com>
2023-06-23 14:37:21 +0800

    add review routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const routes = (app) => {
   app.use("/api/thumbnail", require("../routes/thumbnail.routes"));
   app.use("/api/transport", require("../routes/transport.routes"));
   app.use("/api/contact", require("../routes/contact.routes"));
+  app.use("/api/review", require("../routes/review.routes"));
 };
 
 module.exports = routes;

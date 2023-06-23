055d011e7d5f8a27c0fc07022829c959da9f0210
Ari Wiradana <ariwiradana5@gmail.com>
2023-06-23 14:41:10 +0800

    add created_at payload

--- controller/review.controller.js
+++ controller/review.controller.js
@@ -10,10 +10,10 @@ const getReview = async (req, res) => {
 };
 
 const setReview = async (req, res) => {
-  const { rating, name, content } = req.body;
+  const { rating, name, content, created_at } = req.body;
 
   try {
-    const newReview = await Review.create({ rating, name, content });
+    const newReview = await Review.create({ rating, name, content, created_at});
     res.status(200).json(newReview);
   } catch (error) {
     res.status(500).json(error);

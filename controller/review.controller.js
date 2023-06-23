ce10da54c516a689b339b8ffaf7632c9966e44df
Ari Wiradana <ariwiradana5@gmail.com>
2023-06-23 14:42:37 +0800

    change to find all get review

--- controller/review.controller.js
+++ controller/review.controller.js
@@ -2,7 +2,7 @@ const Review = require("../model/review.model");
 
 const getReview = async (req, res) => {
   try {
-    const data = await Review.findOne();
+    const data = await Review.findAll();
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json(error);

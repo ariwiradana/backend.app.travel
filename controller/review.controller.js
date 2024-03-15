262e5996ce8c3e03a22f8ed47b79491926fedb3a
Ari Wiradana <ariwiradana5@gmail.com>
2024-03-15 11:07:27 +0800

    updated

--- controller/review.controller.js
+++ controller/review.controller.js
@@ -1,9 +1,10 @@
 const Review = require("../model/review.model");
+const sanitizeHtml = require("sanitize-html");
 
 const getReview = async (req, res) => {
   const { limit } = req.query;
   try {
-    let data
+    let data;
     if (limit) {
       data = await Review.find().limit(Number(limit));
     } else {
@@ -19,14 +20,31 @@ const setReview = async (req, res) => {
   const { rating, name, review, created_at } = req.body;
 
   try {
-    const newReview = await Review.create({ rating, name, review, created_at });
+    const newReview = await Review.create({
+      rating,
+      name: sanitizeHtml(name),
+      review: sanitizeHtml(review),
+      created_at,
+    });
     res.status(200).json(newReview);
   } catch (error) {
     res.status(500).json(error);
   }
 };
 
+const deleteReview = async (req, res) => {
+  const { _id } = req.params;
+  try {
+    const deletedReview = await Review.deleteOne({ _id });
+    res.status(200).json(deletedReview);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getReview,
   setReview,
+  deleteReview,
 };
--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getReview, setReview } = require("../controller/review.controller");
+const {
+  getReview,
+  setReview,
+  deleteReview,
+} = require("../controller/review.controller");
 
 router.route("/").get(getReview).post(setReview);
+router.route("/:_id").post(deleteReview);
 module.exports = router;
